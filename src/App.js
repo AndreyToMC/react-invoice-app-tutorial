17892c55d7d87f36e5b35e335e073a85bccca86e
AndretyToMC <andrey.m@tomuchcoffee.com>
2018-08-31 13:18:05 +0300

    first dumb component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import MySelect from './components/addInvoicePage/selectInput'
+
+// Добавляем наш селект на страницу
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MySelect />
       </div>
     );
   }
---
+++ src/components/addInvoicePage/selectInput.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+
+// Импорт необходимых элементов из Material Ui
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
+
+// Прописываем классы стилей для нашего компонента,
+// особое внимание стоит уделить createStyles - вспомогательная функция для правил стиля (С), необходима для работы под тайпскриптом
+const styles = (theme) => createStyles({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+    width: '100%',
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2,
+  },
+});
+// Прописываем интерфейсы для класса
+// Вполне возможно Тайпскрипт ругается на то, что интерфейсы должен быть с заглавной буквы i, ну пусть будет так...
+// Так же наследуем интерфейс стилей от material Ui
+interface InterfaceMySelect extends WithStyles<typeof styles> {
+  name?: string,
+  values?: [],
+  selected?: number,
+  onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void,
+  placeholder?: string,
+  id?: string,
+  label?: string,
+  errorMsg?: string,
+}
+
+// Создание тупого функционального компонента, потому что нам не нужны state и жизненые циклы компонента реат.
+
+const MySelect = (props: InterfaceMySelect) => {
+  const { classes, name, values, selected, onChange, placeholder, id, label, errorMsg } = props;
+  return (
+    <div className={classes.root}>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor={id} >{label}</InputLabel>
+        <Select
+          error={errorMsg && errorMsg.length > 0}
+          value={selected || ''}
+          onChange={onChange}
+          displayEmpty
+          input={<Input name={name} id='asd' />}
+          autoWidth
+        >
+          <MenuItem value='' disabled>
+            {placeholder}
+          </MenuItem>
+          <MenuItem>one</MenuItem>
+          <MenuItem>two</MenuItem>
+          <MenuItem>three</MenuItem>
+          })}
+        </Select>
+        <FormHelperText error={errorMsg && errorMsg.length > 0} >{errorMsg}</FormHelperText>
+      </FormControl>
+    </div>
+  );
+}
+
+export default withStyles(styles)(MySelect);
